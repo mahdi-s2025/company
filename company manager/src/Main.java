c43fa231c1f808eb1dd2f2b052fb934773e2d9d7
Mahdi <mahdisemsar404@gmail.com>
2024-06-13 01:59:10 +0330

    final test done!

--- company manager/src/Main.java
+++ company manager/src/Main.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
+    public static void main(String[] args) {
+        Admin admin = Admin.getAdmin("Mahdi", "09102244741");
 
+        Employee employee1 = admin.newEmployee("test1", "09100000001", new Date(), 2100, EmployeeType.FULL_TIME_EMPLOYEE);
+        Employee employee2 = admin.newEmployee("test2", "09100000002", new Date(), 2200, EmployeeType.FULL_TIME_EMPLOYEE);
+        Employee employee3 = admin.newEmployee("test3", "09100000003", new Date(), 100, EmployeeType.PART_TIME_EMPLOYEE);
+        Employee employee4 = admin.newEmployee("test4", "09100000004", new Date(), 110, EmployeeType.PART_TIME_EMPLOYEE);
+        Employee employee5 = admin.newEmployee("test5" , "09100000005", new Date(), 700, EmployeeType.PROJECT_EMPLOYEE);
+
+        for (Employee employee : admin.getEmployees()) {
+            employee.work(50);
+            int salary = 0;
+            int amount;
+            do {
+                amount = employee.getSalary();
+                salary += amount;
+            } while (amount != 0);
+            System.out.println(salary);
+        }
+    }
 }
 
 abstract class User {
@@ -159,4 +179,64 @@ final class ProjectEmployee extends Employee {
     public int getProjectSalaryRate() {
         return projectSalaryRate;
     }
+}
+
+class Admin extends User {
+    private static Admin admin;
+    private final ArrayList<Employee> employees;
+
+    private Admin(String fullName, String phoneNumber) {
+        super(fullName, phoneNumber);
+        employees = new ArrayList<>();
+    }
+
+    public static Admin getAdmin(String fullName, String phoneNumber) {
+        if (admin == null) {
+            admin = new Admin(fullName, phoneNumber);
+        }
+        return admin;
+    }
+
+    public static Admin getAdmin() {
+        return admin;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public String getEmployeesInfo() {
+        StringBuilder employeesInfo = new StringBuilder();
+        for (Employee employee : employees) {
+            employeesInfo.append(employee.getBasicInformation()).append("\n");
+        }
+        return employeesInfo.toString();
+    }
+
+    public Employee newEmployee(String fullName, String phoneNumber, Date employmentDate, int salaryRate, EmployeeType employeeType) {
+        switch (employeeType) {
+            case FULL_TIME_EMPLOYEE -> {
+                FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(fullName, phoneNumber, employmentDate, salaryRate);
+                employees.add(fullTimeEmployee);
+                return fullTimeEmployee;
+            }
+            case PART_TIME_EMPLOYEE -> {
+                PartTimeEmployee partTimeEmployee = new PartTimeEmployee(fullName, phoneNumber, employmentDate, salaryRate);
+                employees.add(partTimeEmployee);
+                return partTimeEmployee;
+            }
+            case PROJECT_EMPLOYEE -> {
+                ProjectEmployee projectEmployee = new ProjectEmployee(fullName, phoneNumber, employmentDate, salaryRate);
+                employees.add(projectEmployee);
+                return projectEmployee;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+}
+
+enum EmployeeType {
+    FULL_TIME_EMPLOYEE, PART_TIME_EMPLOYEE, PROJECT_EMPLOYEE
 }
